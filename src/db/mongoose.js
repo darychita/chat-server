df8f7818cd8ac9a15f7593db6b6b16600ec483f7
Darina Chitadze <ms.darri2003@gmail.com>
2019-07-21 22:31:47 +0300

    remote

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/chat-api", {
+// mongoose.connect("mongodb://127.0.0.1:27017/chat-api", {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: true
+// });
+
+
+mongoose.connect("mongodb+srv://mainUser:j9bDtLxZniGHgMeS@cluster0-4o0fs.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ require("./db/mongoose");
 const User = require("./model/User");
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // app.use(basicAuth)
